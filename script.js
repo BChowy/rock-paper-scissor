fefd20fdfe27b291d16ace786eb52c3c507f8845
Bushra <75831792+BChowy@users.noreply.github.com>
2023-10-25 19:57:03 +0300

    Change from rock-paper-scissor to The wizards' war
    - changed to stone-wind-fireball
    - added details for the reset button
    - removed rounds

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let computerScore = 0;
 let playerScore = 0;
-let numOfRounds = 0;
 
-const rounds = document.querySelector('#round');
 const gameSect = document.querySelector('#game');
-const options = document.querySelector('#options');
+const options = document.querySelector('#playerChoice');
+const CPUChoice = document.querySelector('#computerChoice')
 const buttons = options.querySelectorAll('button');
 const result = document.querySelector('#result');
 
-const resetBtn = document.querySelector('reset');
-resetBtn.style.display = 'none';
+const choices = document.querySelector('#choices');
+const resetBtn = document.querySelector('#reset');
 
 let player = document.querySelector('#playerScore');
 let computer = document.querySelector('#computerScore');
@@ -22,14 +21,24 @@ buttons.forEach((button) => {
 
         singleRound(getComputerChoice(), playerChoice);
 
-        if(playerScore == 5 || computerScore == 5)
-        winner();
+        if (playerScore == 5 || computerScore == 5)
+            winner();
     });
+});
+
+resetBtn.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    result.textContent = 'Choose a spell to cast';
+    CPUChoice.textContent = 'opponent spell';
+
+    choices.classList.toggle('hide-element');
+    resetBtn.classList.toggle('hide-element');
 });
 
 // Return a random computer choice
 function getComputerChoice() {
-    const choiceList = ['ROCK', 'PAPER', 'SCISSOR'];
+    const choiceList = ['STONE', 'FIREBALL', 'WIND'];
     let randomChoice = choiceList[Math.floor(Math.random() * choiceList.length)];
 
     return randomChoice;
@@ -37,48 +46,40 @@ function getComputerChoice() {
 
 // Show the result of one round, change the scores, and display the new score 
 function singleRound(computerChoice, playerChoice) {
-    numOfRounds += 1;
+    CPUChoice.textContent = computerChoice;
+
     if (computerChoice === playerChoice) {
-        result.textContent = 'It\'s a draw!';
-        //console.log("It's a draw!");
+        result.textContent = `Two of ${computerChoice}'s it's a draw!`;
     }
 
-    else if (computerChoice === 'ROCK' && playerChoice === "SCISSOR" || computerChoice === 'PAPER' && playerChoice === "ROCK" || computerChoice === 'SCISSOR' && playerChoice === "PAPER") {
-        //console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+    else if (computerChoice === 'STONE' && playerChoice === "WIND" || computerChoice === 'FIREBALL' && playerChoice === "STONE" || computerChoice === 'WIND' && playerChoice === "FIREBALL") {
         result.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
         computerScore += 1;
-
     }
 
-    else if (playerChoice === 'ROCK' && computerChoice === "SCISSOR" || playerChoice === 'PAPER' && computerChoice === "ROCK" || playerChoice === 'SCISSOR' && computerChoice === "PAPER") {
-        //console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+    else if (playerChoice === 'STONE' && computerChoice === "WIND" || playerChoice === 'FIREBALL' && computerChoice === "STONE" || playerChoice === 'WIND' && computerChoice === "FIREBALL") {
         result.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
         playerScore += 1;
     }
 
     player.textContent = playerScore;
     computer.textContent = computerScore;
-    rounds.textContent = numOfRounds;
 
 }
 
 // Declare the final winner, and reset scores
-function winner(){
-if (playerScore > computerScore) {
-    result.textContent = 'YOU\'VE WON'
-    // console.log('YOU\'VE WIN!');
-}
+function winner() {
+    choices.classList.toggle('hide-element');
+    resetBtn.classList.toggle('hide-element');
 
-else {
-    result.textContent = 'YOU\'VE LOST'
-    // console.log('YOU\'VE LOST')
-}
+    if (playerScore > computerScore) {
+        result.textContent = 'YOU\'VE WON. Now what?'
+        // console.log('YOU\'VE WIN!');
+    }
 
-playerScore = 0;
-computerScore = 0;
-numOfRounds = 0;
-}
-
-function reset(){
+    else {
+        result.textContent = 'YOU\'VE LOST. Better find something else to do'
+        // console.log('YOU\'VE LOST')
+    }
 
 }
