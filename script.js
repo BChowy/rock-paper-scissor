da2a7e580ead2c4e4ae5f55b7f4d74780b631ef8
Bushra <75831792+BChowy@users.noreply.github.com>
2023-11-01 00:25:47 +0300

    Add colors to the result
    - Red in case of losing
    - Lightseagreen in case of winning
    - white when it's a draw

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ resetBtn.addEventListener('click', () => {
     computerScore = 5;
     result.textContent = 'Choose a spell to cast';
     CPUChoice.textContent = 'opponent spell';
+    result.style.color = 'white';
 
     choices.classList.toggle('hide-element');
     resetBtn.classList.toggle('hide-element');
@@ -50,15 +51,19 @@ function singleRound(computerChoice, playerChoice) {
 
     if (computerChoice === playerChoice) {
         result.textContent = `Two of ${computerChoice}'s it's a draw!`;
+        result.style.color = 'white';
     }
 
     else if (computerChoice === 'STONE' && playerChoice === "WIND" || computerChoice === 'FIREBALL' && playerChoice === "STONE" || computerChoice === 'WIND' && playerChoice === "FIREBALL") {
         result.textContent = `You lose! ${computerChoice} beats ${playerChoice}`;
+        result.style.color = 'red';
         computerScore -= 1;
+        
     }
 
     else if (playerChoice === 'STONE' && computerChoice === "WIND" || playerChoice === 'FIREBALL' && computerChoice === "STONE" || playerChoice === 'WIND' && computerChoice === "FIREBALL") {
         result.textContent = `You win! ${playerChoice} beats ${computerChoice}`;
+        result.style.color = 'lightseagreen';
         playerScore -= 1;
     }
 
@@ -73,13 +78,13 @@ function winner() {
     resetBtn.classList.toggle('hide-element');
 
     if (computerScore == 0) {
-        result.textContent = 'YOU\'VE WON. Now what?'
-        // console.log('YOU\'VE WIN!');
+        result.textContent = 'YOU\'VE WON. Now what?';
+        result.style.color = 'lightseagreen';
     }
 
     else {
-        result.textContent = 'YOU\'VE LOST. Better find something else to do'
-        // console.log('YOU\'VE LOST')
+        result.textContent = 'YOU\'VE LOST. Better find something else to do';
+        result.style.color = 'red';
     }
 
 }
